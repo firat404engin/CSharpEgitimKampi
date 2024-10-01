d9212fb030b520b7a580b19a1365424d5872cda3
firatengin404 <62977294+firat404engin@users.noreply.github.com>
2024-10-02 01:34:10 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Console.WriteLine("Merhaba Dünya");
+			//Console.Read();
+
+			#region YazdırmaKomutları
+			//Console.WriteLine("***** Yemek Kategorileri ******");
+			//Console.WriteLine();
+			//Console.WriteLine("1- Çorbalar");
+			//Console.WriteLine("2- Ana Yemekler");
+			//Console.WriteLine("3- Soğuk Başlangıçlar");
+			//Console.WriteLine("4- Salatalar");
+			//Console.WriteLine("5- Tatlılar");
+			#endregion
+
+			#region String Değişkenler
+			//string name;
+			//name = "Firat";
+			//         Console.WriteLine(name);
+
+
+			//string customerName;
+			//string customerSurname;
+			//string customerPhone;
+			//string customerEmail, district, city;
+
+			//customerName = "Firat";
+			//customerSurname = "Engin";
+			//customerPhone = "+90 0536 055 7721";
+			//customerEmail = "fengin7321@gmail.com";
+			//district = "Çekmeköy";
+			//city = "Istanbul";
+
+			//         Console.WriteLine("**** Rezervasyo Kartı ****");
+			//Console.WriteLine("---------------------------");
+			//         Console.WriteLine(customerName);
+			//         Console.WriteLine(customerSurname);
+			//         Console.WriteLine(customerPhone);
+			//         Console.WriteLine(customerEmail);
+			//         Console.WriteLine(district);
+			//         Console.WriteLine(city);
+			//Console.WriteLine("---------------------------");
+
+			//customerName = "Furkan";
+			//customerSurname = "Kurt";
+			//customerPhone = "+90 0536 055 1231";
+			//customerEmail = "cfk@gmail.com";
+			//district = "Çan";
+			//city = "Çanakkale";
+
+			//Console.WriteLine(customerName);
+			//Console.WriteLine(customerSurname);
+			//Console.WriteLine(customerPhone);
+			//Console.WriteLine(customerEmail);
+			//Console.WriteLine(district);
+			//Console.WriteLine(city);
+			//Console.WriteLine("---------------------------");
+
+
+			#endregion
+
+			#region Int Değişkenler
+
+			//int number = 73;
+			//         Console.WriteLine(number);
+
+			//int hamburgerPrice = 300;
+			//int cokePrice = 35;
+			//int waterPrice = 10;
+			//int friesPrice = 50;
+			//int pizzaPrice = 250;
+			//int lemonadePrice = 30;
+
+   //         Console.WriteLine("**** Restorant Menü Fiyatı ****");
+   //         Console.WriteLine();
+   //         Console.WriteLine("-- Hamburger Fiyatı: "+ hamburgerPrice + "TL");
+   //         Console.WriteLine("-- Pizza Fiyatı: "+ cokePrice + "TL");
+   //         Console.WriteLine("-- Su Fiyatı: "+ waterPrice + "TL");
+   //         Console.WriteLine("-- Patates Kızartması: "+ friesPrice + "TL");
+   //         Console.WriteLine("-- Pizza Fiyatı: "+ pizzaPrice + "TL");
+   //         Console.WriteLine("-- Limonata Fiyatı: "+ lemonadePrice + "TL");
+   //         Console.WriteLine();
+			//Console.WriteLine("**** Restorant Menü Fiyatı ****");
+
+   //         Console.WriteLine();
+			//int hamburgerCount =3;
+			//int cokeCount=3;
+			//int waterCount=3;
+			//int friesCount=5;
+			//int pizzaCount=0;
+			//int lemonadeCount=2;
+
+			//int totelPrice = hamburgerCount*hamburgerPrice + cokeCount*cokePrice + waterCount * waterPrice + friesCount *friesPrice + pizzaCount*pizzaPrice+lemonadeCount*lemonadePrice;
+
+   //         Console.WriteLine("Toplam hamburger adet:" + hamburgerCount );
+   //         Console.WriteLine("Toplam kola adet:" + cokeCount);
+   //         Console.WriteLine("Toplam su adet:" + waterCount);
+   //         Console.WriteLine("Toplam patates  adet:" + friesCount);
+   //         Console.WriteLine("Toplam pizza  adet:" + pizzaCount);
+   //         Console.WriteLine("Toplam limonata  adet:" + lemonadeCount);
+   //         Console.WriteLine("Toplam Ücret "+ totelPrice);
+
+			#endregion
+
+			Console.Read();
+
+		}
+	}
+}
